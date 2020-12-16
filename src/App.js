b8b7d88d8636d2be3f14619f6e266645ba3c4f2c
Farhan Ul Haq <farhan@openvpn.net>
2020-12-15 21:55:49 -0800

    feat: Replace state-based routing with React Router

--- src/App.js
+++ src/App.js
@@ -3,17 +3,10 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookList from './BookList'
 import Search from './Search'
-import './App.css'
+import { Route, Link } from 'react-router-dom';
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: []
   }
 
@@ -26,22 +19,22 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <Search />
-        ) : (
+        <Route exact path='/search' render={() => <Search />} />
+        <Route exact path='/' render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
             <BookList books={this.state.books}/>
             <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+              <Link to='/search'>
+                <button>Add a book</button>
+            </Link>
             </div>
           </div>
-        )}
-      </div>
-    )
-  }
+        )} />
+    </div>
+    )}
 }
 
 export default BooksApp
