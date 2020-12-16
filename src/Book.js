2cd88bcceeb3bba337e3ad1be54660be69f47a9b
Farhan Ul Haq <farhan@openvpn.net>
2020-12-16 14:26:02 -0800

    feat: Implement Search feature

--- src/Book.js
+++ src/Book.js
@@ -5,10 +5,11 @@ import ShelfChanger from './ShelfChanger'
 class Book extends Component {
   render() {
     const book = this.props.book
+
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.url}")` }}></div>
           <ShelfChanger />
         </div>
         <div className="book-title">{book.title}</div>
--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Book from './Book'
+import {DebounceInput} from 'react-debounce-input';
+
+import * as BooksAPI from './BooksAPI'
 
 class Search extends Component {
+  state = {
+    search: '', // the search query that's typed in the search bar
+    books:  [], // The books returned as a result of the query
+  }
+
+  updateSearch = (q) => {
+    return BooksAPI.search(q.trim()).then((result) => {
+      (!result || result.error) ? this.setState({search: q, books: []})
+                                : this.setState({search: q, books: result});
+    })
+  }
+
+  componentDidMount() {
+    this.searchInput.focus();
+  }
+
   render() {
+    const { search, books } = this.state;
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -10,15 +32,26 @@ class Search extends Component {
             <button className="close-search">Close</button>
           </Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            {/* Use DebounceInput for a smoother search experience  */}
+            <DebounceInput
+              inputRef={(input) => { this.searchInput = input; }}
+              minLength={0}
+              debounceTimeout={400}
+              value={search} onChange={(event) => this.updateSearch(event.target.value)} />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {books.map((b) => (
+              <li key={b.id}>
+                <Book book={b} />
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
-    );
-  }
+      );
+    }
 }
 
 export default Search;
