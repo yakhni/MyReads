a07c7adeaaa209b75dda68e2d7296ca477794516
Farhan Ul Haq <farhan@openvpn.net>
2020-12-18 08:08:57 -0800

    fix: handle how to render book without thumbnail. Simply use '#' as url for thumbnail, if thumbnail not found in book record.

--- src/Book.js
+++ src/Book.js
@@ -6,10 +6,18 @@ class Book extends Component {
   render() {
     const book = this.props.book
 
+    let url = '#'
+    try {
+      url = this.props.book.imageLinks.thumbnail ;
+    } catch (error) {
+      // NOTE: if I search for 'b', BooksAPI returns a book without a thumbnail
+      // There's no need to do anything about it, since CSS already creates an empty border for us
+    }
+
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.url}")` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${url}")` }}></div>
           <ShelfChanger />
         </div>
         <div className="book-title">{book.title}</div>
